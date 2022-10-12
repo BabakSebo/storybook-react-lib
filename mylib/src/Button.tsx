97381cf518b646a6bebed9d860a1818f34996775
Babak Arjomand <babak@Babaks-MacBook-Air.local>
2022-10-12 20:25:53 +0100

    introduced styling components for Button

--- mylib/src/Button.tsx
+++ mylib/src/Button.tsx
@@ -1,4 +1,28 @@
 import React, { HTMLAttributes, ReactNode } from 'react';
+import styled from 'styled-components';
+import { variant } from 'styled-system';
+
+const ThemedButton = styled('button')(
+  {
+    color: 'black',
+    fontWeight: 'bold',
+    border: 'none',
+    borderRadius: 10,
+    padding: 20,
+    cursor: 'pointer',
+  },
+  variant({
+    variants: {
+      primary: {
+        bg: 'rgba(245, 194, 68, 255)',
+      },
+      secondary: {
+        color: 'white',
+        bg: 'skyblue',
+      },
+    },
+  })
+);
 
 export interface Props extends HTMLAttributes<HTMLButtonElement> {
   /** Provide text for the button */
@@ -9,22 +33,32 @@ export interface Props extends HTMLAttributes<HTMLButtonElement> {
   variant: 'primary' | 'secondary';
 }
 
-export const Button = ({ children, variant = 'primary', ...props }: Props) => {
-  return (
-    <button
-      {...props}
-      style={{
-        backgroundColor:
-          variant === 'primary' ? 'rgba(245,194,68,255)' : 'skyblue',
-        color: 'black',
-        fontWeight: 'bold',
-        border: 'none',
-        borderRadius: 10,
-        padding: 20,
-        cursor: 'pointer',
-      }}
-    >
-      {children}
-    </button>
-  );
+export const Button = ({ children, ...props }: Props) => {
+  return <ThemedButton {...props}>{children}</ThemedButton>;
 };
+
+// style={{
+//     backgroundColor:
+//       variant === 'primary' ? 'rgba(245,194,68,255)' : 'skyblue',
+//     color: 'black',
+//     fontWeight: 'bold',
+//     border: 'none',
+//     borderRadius: 10,
+//     padding: 20,
+//     cursor: 'pointer',
+//   }}
+
+// const backgroundColor = theme.variants('mode', 'variant', {
+//     primary: { light: 'rgba(245,194,68,255)', dark: 'rgba(245,194,68,255)' },
+//     secondary: { light: 'skyblue', dark: 'skyblue' },
+//   });
+
+//   const ThemeButton = styled.button`
+//     color: black;
+//     background-color: ${backgroundColor};
+//     font-weight: bold;
+//     border: none;
+//     border-radius: 10px;
+//     padding: 20px;
+//     cursor: pointer;
+//   `;
